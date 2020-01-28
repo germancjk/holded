1a77df541e942fa27b40d8af84ce0894a5cf9c01
German Gonzalez <gmgonzalez@mgc.es>
2020-01-28 12:40:45 +0100

    init

---
+++ resources/js/app.js
@@ -0,0 +1,47 @@
+import Vue          from 'vue'
+import VueRouter    from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App          from './views/App'
+import Dashboard    from './views/Board'
+import Login        from './views/Login'
+import Register     from './views/Register'
+import Home         from './views/Welcome'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+        },
+        {
+            path: '/board',
+            name: 'board',
+            component: Dashboard,
+        },
+        {
+            path: '/logout',
+            name: 'logout',
+            component: Login,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ resources/js/views/Board.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+          <ul>
+            <li v-for="element,index in categories">{{element.name}}</li>
+          </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    border:0;
+    border-radius: 0.5rem;
+}
+.transit-1 {
+    transition: all 1s;
+}
+.small-card {
+    padding: 1rem;
+    background: #f5f8fa;
+    margin-bottom: 5px;
+    border-radius: .25rem;
+}
+.card-body-dark{
+    background-color: #ccc;
+}
+textarea {
+    overflow: visible;
+    outline: 1px dashed black;
+    border: 0;
+    padding: 6px 0 2px 8px;
+    width: 100%;
+    height: 100%;
+    resize: none;
+}
+</style>
+
+<script>
+export default {
+    components: {
+        //
+    },
+    data(){
+        return {
+            categories : [],
+            editingTask : null
+        }
+    },
+    methods : {
+        addNew(id) {
+            let user_id = 1
+            let name = "New task"
+            let category_id = this.categories[id].id
+            let order = this.categories[id].tasks.length
+
+            axios.post('api/task', {user_id, name, order, category_id}).then(response => {
+                this.categories[id].tasks.push(response.data.data)
+            })
+        },
+        loadTasks() {
+          this.categories.map(category => {
+              axios.get(`api/category/${category.id}/tasks`).then(response => {
+                  category.tasks = response.data
+              })
+          })
+        },
+        changeOrder(data){
+        },
+        endEditing(task) {
+            this.editingTask = null
+
+            axios.patch(`api/task/${task.id}`, {name: task.name}).then(response => {
+                // You can do anything you wan't here.
+            })
+        },
+        editTask(task){
+            this.editingTask = task
+        }
+    },
+    mounted() {
+        let token = localStorage.getItem('jwt')
+
+        axios.defaults.headers.common['Content-Type'] = 'application/json'
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+
+        axios.get('api/category').then(response => {
+            response.data.forEach((data) => {
+                this.categories.push({
+                    id : data.id,
+                    name : data.name,
+                    tasks : []
+                })
+            })
+            this.loadTasks()
+        })
+    },
+    computed: {
+    }
+}
+</script>
